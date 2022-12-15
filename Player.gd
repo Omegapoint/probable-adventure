2f80393aefb1e5a3d517f63bc4a7fd3e5be60535
Isak Pettersson <isakpettersson@Isaks-MacBook-Pro.local>
2022-12-15 17:01:15 +0100

    added speed dash test and smaller ball

--- Player.gd
+++ Player.gd
@@ -4,11 +4,13 @@ var screen_size
 var moveRight
 var moveLeft
 var moveUp
-
+var characterSpeed
 var id = 1 # Just to have a default for running this script solo
+var speedDashCooldown = false
+var speedDashCounter = 0
 
-var isBounce = false
-var bounceCounter = 0
+
+
 
 # Called when the node enters the scene tree for the first time.
 # Here we can set unique stats for different characters
@@ -21,19 +23,19 @@ func _ready():
 		moveLeft = "moveLeft_arrow"
 		moveUp = "moveUp_arrow"
 		$AnimatedSprite.animation = "rabbit"
-		speed = 300
+		characterSpeed = 300
 	elif (id == 2):
 		moveRight = "moveRight_D"
 		moveLeft = "moveLeft_A"
 		moveUp = "moveUp_W"
 		$AnimatedSprite.animation = "giraffe"
-		speed = 300
+		characterSpeed= 300
 	else:
 		moveRight = "moveRight_M"
 		moveLeft = "moveLeft_N"
 		moveUp = "moveUp_J"
 		$AnimatedSprite.animation = "parrot"
-		speed = 300
+		characterSpeed = 300
 	
 	screen_size = get_viewport_rect().size
 
@@ -44,32 +46,44 @@ func _on_Player_body_entered(body):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	$ShootArea.disabled = true
+	speed = characterSpeed
 	
+	var temp_velocity
 	var velocity = Vector2.ZERO # The player's movement vector.
 	var forward_face_direction = Vector2(cos(rotation), sin(rotation))
 	velocity.x += 1 * forward_face_direction.x
 	velocity.y += 1 * forward_face_direction.y
 	
 	
-	if !isBounce:
-		if Input.is_action_pressed(moveRight):
-			rotation += 0.05
-			
-		if Input.is_action_pressed(moveLeft):
-			rotation -= 0.05
-		
-		if Input.is_action_pressed(moveUp):
-			$ShootArea.disabled = false
-		
-	else:
-		if bounceCounter > 10:
-			isBounce = false
-			bounceCounter = 0
-		else:
-			bounceCounter += 1
+	# Player controls 	
+	if Input.is_action_pressed(moveRight):
+		rotation += 0.05
 
-	
+	if Input.is_action_pressed(moveLeft):
+		rotation -= 0.05
+
+	if Input.is_action_pressed(moveUp):
+		
+		if !speedDashCooldown:
+			speed = 2000
+			if speedDashCounter >= 50:
+				speedDashCooldown = true 
+				speedDashCounter += 1
+		else:
+			if speedDashCounter >= 0:
+				speedDashCounter -= 1
+			else:
+				speedDashCooldown = false
+			
+				
+	# Bounce 
+	if velocity.x !=0:
+		temp_velocity = velocity.x
+	for i in range(get_slide_count()):
+		var collision = get_slide_collision(i)
+		if collision.collider is TileMap and not is_on_floor():
+			velocity.x = collision.normal.x*abs(temp_velocity)*0.6
+
 	# Animation 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -82,4 +96,3 @@ func _process(delta):
 		velocity.x = velocity.x * (-1)
 		velocity.y = velocity.y * (-1)
 		rotation = (rotation) + (PI)
-		isBounce = true
