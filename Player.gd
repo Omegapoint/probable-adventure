fe5d032aac70e4f7bf03d1db6ccbbb899377f30d
Isak Pettersson <isakpettersson@Isaks-MacBook-Pro.local>
2022-12-15 13:23:48 +0100

    implemented bounce in player, against other players and wall

--- Player.gd
+++ Player.gd
@@ -1,9 +1,12 @@
-extends Area2D
+extends KinematicBody2D
 export var speed = 100 # How fast the player will move (pixels/sec).
 var screen_size
 var moveRight
 var moveLeft
 var id = 1 # Just to have a default for running this script solo
+
+var isBounce = false
+var bounceCounter = 0
 
 # Called when the node enters the scene tree for the first time.
 # Here we can set unique stats for different characters
@@ -31,28 +34,39 @@ func _on_Player_body_entered(body):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	var velocity = Vector2.ZERO # The player's movement vector.
 	var forward_face_direction = Vector2(cos(rotation), sin(rotation))
 	velocity.x += 1 * forward_face_direction.x
 	velocity.y += 1 * forward_face_direction.y
 	
-	if Input.is_action_pressed(moveRight):
-		rotation += 0.05
+	var collide = move_and_collide(velocity * delta)
+	if collide:
+		velocity.x = velocity.x * (-1)
+		velocity.y = velocity.y * (-1)
+		rotation = (rotation) + (PI)
+		isBounce = true
+	
+	if !isBounce:
+		if Input.is_action_pressed(moveRight):
+			rotation += 0.05
+			
+		if Input.is_action_pressed(moveLeft):
+			rotation -= 0.05
 		
-	if Input.is_action_pressed(moveLeft):
-		rotation -= 0.05
-	
-	# Animation 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
+		# Animation 
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite.play()
+		else:
+			$AnimatedSprite.stop()
+		
 	else:
-		$AnimatedSprite.stop()
+		if bounceCounter > 10:
+			isBounce = false
+			bounceCounter = 0
+		else:
+			bounceCounter += 1
 	
-	# Set position of player, clamp to make sure player can't move out of bounds
+	# Set new position
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-
-
-
