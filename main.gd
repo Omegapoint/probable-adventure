32b1a4a7047ce5ef0d7a750b08a07174bd153b7d
Isak Pettersson <isakpettersson@Isaks-MacBook-Pro.local>
2022-12-16 15:47:11 +0100

    added visual updates

--- main.gd
+++ main.gd
@@ -3,6 +3,20 @@ extends Node2D
 var player = preload("res://Player.tscn")
 var nrOfPlayers = 8
 var playerList = []
+var playerCoordinateList = [
+	# Team blue
+	[150,540],
+	[450,890],
+	[450,190],
+	[750,540],
+	
+	# Team brown
+	[1770,540],
+	[1470,890],
+	[1470,190],
+	[1170,540]
+	]
+
 
 # Map boundary 
 var mapX
@@ -15,16 +29,15 @@ func _ready():
 	mapY = int(mapSize.y)
 	
 	for i in nrOfPlayers:
-		create_player(i + 1)
+		var coordinate = playerCoordinateList[i]
+		create_player(i + 1,coordinate[0],coordinate[1])
 
-func create_player(id):
+func create_player(id,x,y):
 	var player_instance = player.instance()
 	player_instance.id = id
 
-	# Randomize start position of new player
 	# Add list of positions and go through it instead
-	randomize()
-	player_instance.position = Vector2(randi()%mapX,randi()%mapY)
+	player_instance.position = Vector2(x,y)
 
 	# Add player to the main scene and append to player-list
 	add_child(player_instance)
