60cb547e32caad7c43d3c2e2640d38b9f5b86245
salemxd <uzumaki.salem@gmail.com>
2022-12-29 13:24:52 +0100

    added glow to powerups

--- scenes/PowerUpSurprise.gd
+++ scenes/PowerUpSurprise.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends StaticBody2D
 
 
 # Declare member variables here. Examples:
@@ -15,8 +15,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Area2D_body_entered(body):
-	print("heeeeeeeeej")
-	pass # Replace with function body.
